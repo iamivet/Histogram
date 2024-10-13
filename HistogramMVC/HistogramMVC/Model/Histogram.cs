964d01ec087ab03488f9c9cb24a2ce36bfc2b6a4
Ivet Dimitrova <121934923+iamivet@users.noreply.github.com>
2024-10-13 19:27:17 +0300

    Add

---
+++ HistogramMVC/HistogramMVC/Model/Histogram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistogramMVC.Model
+{
+    public class Histogram
+    {
+        public int[] Numbers { get; }
+
+        public int P1 { get; private set; }
+        public int P2 { get; private set; }
+        public int P3 { get; private set; }
+        public int P4 { get; private set; }
+        public int P5 { get; private set; }
+
+        public Histogram(int[] numbers)
+        {
+            this.Numbers = numbers;
+        }
+        public void CalculateCount()
+        {
+            foreach (int number in this.Numbers)
+            {
+                if (number < 200)
+                {
+                    this.P1++;
+                }
+                else if (number < 400)
+                {
+                    this.P2++;
+                }
+                else if (number < 600)
+                {
+                    this.P3++;
+                }
+                else if (number < 800)
+                {
+                    this.P4++;
+                }
+                else
+                {
+                    this.P5++;
+                }
+            }
+        }
+    }
+}
---
+++ HistogramMVC/HistogramMVC/View/Display.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HistogramMVC.View
+{
+    public class Display
+    {
+        public int[] Numbers { get; set; }
+
+        public  int P1 { get; set; }
+        public int P2 { get; set; }
+        public int P3 { get; set; }
+        public int P4 { get; set; }
+        public int P5 { get; set; }
+
+        public void Input()
+        {
+            //    Console.WriteLine("Въведи редица от числа:");
+            //    this.Numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int count = int.Parse(Console.ReadLine());
+            int[] numbers = new int[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            this.Numbers = numbers;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine($"Числата , които са под 200 са:{(double)this.P1 / this.Numbers.Length * 100:f2}%");
+            Console.WriteLine($"Числата , които са от 200 до 399 са:{(double)this.P2 / this.Numbers.Length * 100:f2}%");
+            Console.WriteLine($"Числата , които са от 400 до 599 са:{(double)this.P3 / this.Numbers.Length * 100:f2}%");
+            Console.WriteLine($"Числата , които са от 600 до 799 са:{(double)this.P4 / this.Numbers.Length * 100:f2}%");
+            Console.WriteLine($"Числата , които са от 800 нагоре са:{(double)this.P5 / this.Numbers.Length * 100:f2}%");
+        }
+    }
+}
